ad075f520dba94e05f8a10068cb8a49d6ff96e31
wuhuihao <wuhuihao@qq.com>
2014-08-04 22:48:59 +0800

    菜单树异步加载

--- src/com/whh/controller/MenuController.java
+++ src/com/whh/controller/MenuController.java
@@ -1,11 +1,18 @@
 package com.whh.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
+
+import com.whh.dao.MenuDao;
+import com.whh.model.Menu;
 
 /**
  * Servlet implementation class MenuController
@@ -13,31 +20,40 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/MenuController")
 public class MenuController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MenuController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub\
-		request.setCharacterEncoding("utf-8");
-		String id=request.getParameter("id");
-		response.getWriter().write("{children: [{ text: 'article"+id+"', leaf: true },{ text: 'homework', expanded: true, children: [{ text: 'book report', leaf: true },{ text: 'alegrbra', leaf: true}] },{ text: 'buy lottery tickets', leaf: true }]}");
+	public MenuController() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub\
+		request.setCharacterEncoding("utf-8");
+		String id = request.getParameter("id");
+		List<Menu> list = new MenuDao().findByParentId(id);
+		JSONArray jsonArray=JSONArray.fromObject(list);
+		//JSONObject jsonObject = JSONObject.fromObject(list);
+		//System.out.println(jsonArray.toString());
+		response.getWriter()
+				.write(jsonArray.toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
