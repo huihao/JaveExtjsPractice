acec0cc0bcb65e1f1f91fc6abd307b3bb1263d60
wuhuihao <wuhuihao@qq.com>
2014-08-05 22:49:12 +0800

    grid.panel 多选框  双击修改

--- WebContent/app/view/user/List.js
+++ WebContent/app/view/user/List.js
@@ -1,16 +1,41 @@
 Ext.define('AM.view.user.List',{
 	extend:'Ext.grid.Panel',
 	alias:'widget.userlist',
+	require:[
+	             'Ext.selection.CellModel'
+	         ],
 	
 	initComponent: function() {
         Ext.apply(this, {
         	 store:'User',
-        	 closable:'true',
+        	 columnLines: true,
+        	 selModel: Ext.create('Ext.selection.CheckboxModel'),
         	 columns: [
-        	           { header: 'Name',  dataIndex: 'name' },
-        	           { header: 'Email', dataIndex: 'email', flex: 1 },
-        	           { header: 'Phone', dataIndex: 'phone' }
-        	       ]
+        	           { header: 'Name',  dataIndex: 'name',
+        	        	   editor: {
+        	                   xtype: 'textfield',
+        	                   allowBlank: false
+        	               }
+        	           },
+        	           { header: 'Email', dataIndex: 'email', flex: 1 , 
+        	        	    editor: {
+        	                xtype: 'textfield',
+        	                allowBlank: false
+        	            }
+        	           },
+        	           { header: 'Phone', dataIndex: 'phone',
+        	        	   editor: {
+        	                   xtype: 'textfield',
+        	                   allowBlank: false
+        	               }
+        	           }
+        	       ],
+        	  selType: 'cellmodel',
+        	  plugins: [
+        	            Ext.create('Ext.grid.plugin.RowEditing', {
+        	                clicksToEdit: 1
+        	            })
+        	        ]
         });
         this.callParent(arguments);
         }
